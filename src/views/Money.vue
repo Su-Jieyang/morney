40350483b87dea1b0dd091a4984a24c1c809cfea
Su <1535631290@qq.com>
2021-07-13 20:41:14 +0800

    展示和创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,14 +16,16 @@ import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
-import model from "@/model";
+import recordListModel from "@/models/recordListModel";
+import tagListModel from "@/models/tagListModel";
 import { RecordItem } from "@/recordItem";
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({ components: { Tags, Notes, Types, NumberPad } })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行", "彩票"];
+  tags = tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [],
@@ -39,14 +41,14 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2: RecordItem = model.clone(this.record);
+    const record2: RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
   }
   @Watch("recordList")
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 </script>
