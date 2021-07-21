b6ad2055fe839bf09fa8fd6d94a4d51539c2ea81
Su <1535631290@qq.com>
2021-07-21 22:55:28 +0800

    解决Tag.vue中新增标签按钮的bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -5,7 +5,7 @@
     </div>
     <ul class="current">
       <li
-        v-for="tag in dataSource"
+        v-for="tag in tagList"
         :key="tag.id"
         :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
         @click="toggle(tag)"
@@ -19,9 +19,11 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+import store from "@/store/index2";
+
 @Component
 export default class Tags extends Vue {
-  @Prop() readonly dataSource: string[] | undefined;
+  tagList = store.fetchTags();
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -35,10 +37,10 @@ export default class Tags extends Vue {
   }
   create() {
     const name = window.prompt("请输入标签名：");
-    if (name === "") {
+    if (name) {
+      store.createTag(name);
+    } else if (name === "") {
       window.alert("标签名不能为空");
-    } else if (this.dataSource && name !== null) {
-      this.$emit("update:dataSource", [...this.dataSource, name]);
     }
   }
 }
