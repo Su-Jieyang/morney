c57c4930eca4082d9313da0ae8bd9b23caf9ef27
Su <1535631290@qq.com>
2021-07-26 16:41:30 +0800

    抽离 Tabs.vue，使用在 Statistics.vue 里面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,59 @@
 <template>
-  <div>
-    <Layout>
-      <p>Statistics</p>
-    </Layout>
-  </div>
+  <Layout>
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
+    <Tabs
+      classPrefix="interval"
+      :dataSource="intervalList"
+      :value.sync="interval"
+    />
+    <div>
+      type:{{ type }}
+      <br />
+      interval:{{ interval }}
+    </div>
+  </Layout>
 </template>
 
-<script>
-export default {};
+<script lang='ts'>
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+
+@Component({
+  components: { Tabs },
+})
+export default class Statistics extends Vue {
+  type = "-";
+  interval = "day";
+  intervalList = [
+    {
+      text: "按天",
+      value: "day",
+    },
+    {
+      text: "按周",
+      value: "week",
+    },
+    {
+      text: "按月",
+      value: "month",
+    },
+  ];
+  typeList = [
+    { text: "支出", value: "-" },
+    { text: "收入", value: "+" },
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
+::v-deep .type-tabs-item {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
